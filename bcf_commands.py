45814c2c40ecf10cbda7532eb8e88129e77720f4
Ben Maurer <ben.a.maurer@gmail.com>
2020-11-02 18:00:41 -0600

    Moved bot invite link to reference file

--- bcf_commands.py
+++ bcf_commands.py
@@ -32,7 +32,6 @@ def log_sender(command, sender):
 
 
 #Initial variable declaration
-# https://discord.com/oauth2/authorize?client_id=701463350244278352&scope=bot
 books = ["Genesis", "Exodous", "Leviticus", "Numbers", "Deutoronomy", "Joshua", "Judges", "Ruth", "1 Samuel", "2 Samuel", "1 Kings", "2 Kings", "1 Chronicles", "2 Chronicles", "Ezra", "Nehemiah", "Esther", "Job", "Psalm", "Proverbs", "Ecclesiastes", "Song of Solomon", "Isaiah", "Jeremiah", "Lamentations", "Ezekiel", "Daniel", "Hosea", "Joel", "Amos", "Obadiah", "Jonah", "Micah", "Nahum", "Habakkuk", "Zephaniah", "Haggai", "Zechariah", "Malachi", "Matthew", "Mark", "Luke", "John", "Acts", "Romans", "1 Corinthians", "2 Corinthians", "Galatians", "Ephesians", "Philippians", "Colossians", "1 Thessalonians", "2 Thessalonians", "1 Timothy", "2 Timothy", "Titus", "Philemon", "Hebrews", "James", "1 Peter", "2 Peter", "1 John", "2 John", "3 John", "Jude", "Revelation"]
 line_break = "-------------------------------------------------------------------------\n "
 token = read_token()
